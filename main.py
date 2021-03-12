20bca224857da979c6440412b6180dc0d85fb8d5
Konstantin <akhmadeus@gmail.com>
2021-03-12 16:38:48 +0300

    Added main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Names of tea company employees:
+names = ['Leaf', 'Mailk', 'Cake', 'Jammy', 'Spoonah']
+# Surnames of tea company employees:
+surnames = ['Sugaro', 'Cuppo', 'Creamoff', 'Bru', 'Lipton']
+
+
+
+# Task 1: find the error and fix within branch 'hotfix'
+# Print out the names and surnames of all the five employees
+# in format "Name Surname".
+for i in range(len(names)):
+    print(names[i] + surnames[i])
+
+
+
+
+# Task 2: print all 25 combinations of employees names and surnames.
+# Commit this in branch 'feature' and then merge to 'master'
+
+# Your code goes here ...
