c1b969e9d1d0e8d328074e856b57ec11295a71a7
ecn-foundation-master-sigcoro-2020 <80531629+ecn-foundation-master-sigcoro-2020@users.noreply.github.com>
2021-03-12 17:44:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ names = ['Leaf', 'Mailk', 'Cake', 'Jammy', 'Spoonah']
 surnames = ['Sugaro', 'Cuppo', 'Creamoff', 'Bru', 'Lipton']
 
 
+# ========= Taks 2: Make a hotfix ===========
+# Find the error in the following code and fix within branch 'hotfix'.
 
-# Task 1: find the error and fix within branch 'hotfix'
 # Print out the names and surnames of all the five employees
-# in format "Name Surname".
+# in format "Name Surname". At the moment, for some reason, they are
+# printed as "NameSurname" without a space, which is wrong.
 for i in range(len(names)):
     print(names[i] + surnames[i])
 
 
 
+# ========= Taks 3: Implement a feature ===========
+# Print all 25 combinations of employees' names and surnames.
+# Commit this within branch 'feature' and then merge to 'master'
 
-# Task 2: print all 25 combinations of employees names and surnames.
-# Commit this in branch 'feature' and then merge to 'master'
-
-# Your code goes here ...
+# Your code goes here:
